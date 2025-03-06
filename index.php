c930e780879e874155524ed4ed789db9ef8d268b
docteurwu24 <applouai@gmail.com>
2025-03-06 12:25:01 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+
+		<meta charset=«utf-8»/>
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<title>▄ 𝔻ωᑭℓ𝕒𝕪 ▄</title>
+
+		<link rel="stylesheet" href="CSS/Style.css">
+
+		<link rel="icon" type="Image/jfif" sizes="16x16" href="Image/iconege.jfif">
+
+
+
+			<style>
+
+				body {
+
+  							 background-image: url('Image/Fond-accueil.jpg');
+
+	 						 background-repeat: no-repeat;
+
+	  						 background-attachment: fixed;
+
+ 							 background-size: cover;
+
+ 							 background-position : center;
+
+ 							 cursor: none;
+
+ 							 
+
+						}
+
+			</style>
+
+	</head>
+
+<body>
+
+	<?php include("Inclusion/loader.php"); ?>
+
+	<?php include("Inclusion/menuprincipal.php"); ?>
+
+	
+
+	<h1>
+	  <span class="rainbow-letter" style="--delay: 0s;">D</span>
+	  <span class="rainbow-letter" style="--delay: 0.2s;">W</span>
+	  <span class="rainbow-letter" style="--delay: 0.4s;">P</span>
+	  <span class="rainbow-letter" style="--delay: 0.6s;">L</span>
+	  <span class="rainbow-letter" style="--delay: 0.8s;">A</span>
+	  <span class="rainbow-letter" style="--delay: 1s;">Y</span>
+	</h1>
+
+	<p> Bienvenu dans le site de jeux à choix &#129312
+
+	</p>
+
+	<p>Les jeux :</p>
+
+
+
+
+
+		<p class="center"> 
+
+			<a href="../PHP/Jeux1.php"> <img class="Iconejeux" src="Image/icone1.jpg" > </a> 
+
+		</p>	
+
+	
+
+	<a href="../FAQ.php"> <img class="help" src="Image/aide.gif"> </a>
+
+
+
+   		<script src="Javascript/loader.js"></script>
+
+</body>
+
+</html>
+
