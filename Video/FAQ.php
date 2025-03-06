46d84f7231e4e52ab7bfa4fd7f563498b9ee4516
docteurwu24 <applouai@gmail.com>
2025-03-06 12:27:06 +0100

    Add files via upload

---
+++ Video/FAQ.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset=«utf-8»/>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>▄ 𝔻ωᑭℓ𝕒𝕪 ▄</title>
+		<link rel="stylesheet" href="CSS/Style.css">
+		<link rel="icon" type="image/jfif" sizes="16x16" href="Image/iconege.jfif">
+
+			<style>
+				body {
+  							 background-image: url('../Image/Fond-accueil.jpg');
+	 						 background-repeat: no-repeat;
+	  						 background-attachment: fixed;
+ 							 background-size: cover;
+ 							 background-position : center;
+ 							 cursor: none;
+ 							 
+						}
+			</style>
+	</head>
+<body>
+	<?php include("Inclusion/loader.php"); ?>
+	<?php include("Inclusion/menuprincipal.php"); ?>
+	
+	<h1> <span class="center">𝐅𝐀𝐐</span></h1>
+	
+	<ul>
+		<li>
+			<h3> Y'a t-il des régles particulières ? </h3>
+			<h4> Euhh, oui ? <br>
+			Tu n'as pas le droit de retourner en arrière ...
+			<br> Et c'est tout. </h4>
+		</li>
+		<li>
+			<h3> Pourquoi, je suis parfois rediriger vers un lien externe ? </h3>
+			<h4> C'est des liens qui nous permettent de financer le site &#129303; <br>
+			On fait tout pour que la pub ne te soit pas dérangeante en tout cas. <br>
+			Pour ces liens, il suffira simplement d'attendre 5secondes et tu pourras continuer ton jeu !</h4>
+		</li>
+		<li>
+			<h3> Comment-pourrais je ajouter mon histoire ? &#9997;</h3>
+			<h4> Haha, pour cela à suffit simplement de nous envoyer le script à l'adresse suivante <br>
+			#@gmail.com (pas encore crée) </h4>
+		</li>
+		<li>
+			<h3> Pourrais-je vous soutenir ? </h3>
+			<h4> Ohh, nous avons un petit lien Tipee : (pas encore) <br> </h4>
+		</li>
+		<li>
+			<h3>  <br> <br> Ah oui d'ailleurs, dedicace à moi même @Louai =) <br> Coucou Syrine &#127987;&#65039; (dsl ya pas le drapeau de la Tunisie en HTML - mais ... </h3>
+			<img class="Iconejeux Centrer " src="Image/meme.jpg" align="center" /> 
+			<h4>  </h4>
+		</li>
+	  </h4>
+
+   		<script src="Javascript/loader.js"></script>
+</body>
+</html>
---
+++ Video/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset=«utf-8»/>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>▄ 𝔻ωᑭℓ𝕒𝕪 ▄</title>
+		<link rel="stylesheet" href="CSS/Style.css">
+		<link rel="icon" type="Image/jfif" sizes="16x16" href="Image/iconege.jfif">
+
+			<style>
+				body {
+  							 background-image: url('Image/Fond-accueil.jpg');
+	 						 background-repeat: no-repeat;
+	  						 background-attachment: fixed;
+ 							 background-size: cover;
+ 							 background-position : center;
+ 							 cursor: none;
+ 							 
+						}
+			</style>
+	</head>
+<body>
+	<?php include("Inclusion/loader.php"); ?>
+	<?php include("Inclusion/menuprincipal.php"); ?>
+	
+	<h1> <span class="center">DWplay</span></h1>
+	<p> Bienvenu dans le site de jeux à choix &#129312
+	</p>
+	<p>Les jeux :</p>
+
+
+		<p class="center"> 
+			<a href="../PHP/Jeux1.php"> <img class="Iconejeux" src="Image/icone1.jpg" > </a> 
+		</p>	
+	
+	<a href="../FAQ.php"> <img class="help" src="Image/aide.gif"> </a>
+
+   		<script src="Javascript/loader.js"></script>
+</body>
+</html>
